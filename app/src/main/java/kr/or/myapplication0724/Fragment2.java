259ea438c2ab05b2cdadef621bd0d64718fd0dbc
lullulala <go49239@gmail.com>
2021-07-23 17:38:17 +0900

    안드로이드 앱 커밋

---
+++ app/src/main/java/kr/or/myapplication0724/Fragment2.java
@@ -0,0 +1,25 @@
+package kr.or.myapplication0724;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+        //view객체들 통해서 fragment_2.xml에 배치된 View객체들의 정보를 접근할 수 있다!
+        TextView fg2_tv_data = view.findViewById(R.id.fg2_tv_data);
+
+        fg2_tv_data.setText("두번째 프래그먼트 화면");
+
+        return view;
+    }
+}
